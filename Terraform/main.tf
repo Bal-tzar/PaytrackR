4d4ccbf27e73ccf3eb8308e19ead1a58284dfc36
Otto <ottodemare1@gmail.com>
2025-11-17 10:50:04 +0100

    Added .tf files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,36 @@
 terraform {
     
+    resource "azurerm_resource_group" "main" {
+        name     = var.resource_group_name
+        location = var.location
+    }
+
+    resource "azurerm_kubernetes_cluster" "main" {
+        name                = var.cluster_name
+        location            = azurerm_resource_group.main.location
+        resource_group_name = azurerm_resource_group.main.name
+        dns_prefix          = ""
+        kubernetes_version  = var.kubernetes_version
+
+    default_node_pool {
+        name       = ""
+        node_count = var.node_count
+        vm_size    = var.node_vm_size
+    }
+
+    identity {
+        type = "SystemAssigned"
+  }
+
+    resource "helm_release" "argocd" {
+        name             = "argocd"
+        repository       = "https://argoproj.github.io/argo-helm"
+        chart            = "argo-cd"
+        namespace        = "argocd"
+        create_namespace = true
+        version          = ""
+
+        depends_on = [azurerm_kubernetes_cluster.main]
+ }
+
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,37 @@
 terraform {
+    variable "resource_group_name" {
+        description = "Name of the resource group"
+        type        = string
+        default     = "paytrackr_rg"
+    }   
 
+    variable "location" {
+        description = "Azure region for resources"
+        type        = string
+        default     = "eu-west-1"
+    }
+
+    variable "cluster_name" {
+        description = "Name of the AKS cluster"
+        type        = string
+        default     = "paytrackr_cluster"
+    }
+
+    variable "kubernetes_version" {
+        description = "Kubernetes version"
+        type        = string
+        default     = ""
+    }
+
+    variable "node_count" {
+        description = "Number of nodes in default node pool"
+        type        = number
+        default     = 3
+    }
+
+    variable "node_vm_size" {
+        description = "VM size for nodes"
+        type        = string
+        default     = "small-t1"
+    }
 }
