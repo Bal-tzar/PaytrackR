44c69dfab618007716886c0c45801b4419d54ee1
Bal-tzar <ottodemare1@gmail.com>
2025-11-24 09:15:32 +0100

    new stuff from the laptop

--- Terraform/resource.tf
+++ Terraform/resource.tf
@@ -60,7 +60,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
     network_policy     = "azure"
     service_cidr       = "10.1.0.0/16"
     dns_service_ip     = "10.1.0.10"
-    docker_bridge_cidr = "172.17.0.1/16"
   }
 
   tags = {
